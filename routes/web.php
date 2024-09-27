a0cbf44cb4674a8a5e475090efd7c9c3ea6a5be5
lehoangtran <hoangtran22072004@gmail.com>
2024-09-27 09:57:25 +0700

    update lan 2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 
+
+//frontend
 Route::get('/', function () {
-    return view('welcome');
+    return view('layout');
 });
+
+Route::get('/trang_chu', [HomeController::class, 'index'])->name('home');
+//backend
+//Route::get('/admin', 'AdminController@index')->name('admin');
+
+
+Route::get('/admin', [AdminController::class, 'index'])->name('admin');
